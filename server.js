068c91532b1aa246e401ba9967094e6bc0a27566
Chris Keathley <c@keathley.io>
2015-07-02 11:07:02 -0400

    Speed up rebundles
    This speeds up rebundles in a few ways:
    
    1) We now use up to date versions of all of our dependencies.
    2) Stop producing sourcemaps on every build.  We are now using eval
    which is much faster.
    3) Convert from ruby sass to lib-sass.  lib-sass still has a few kinks
    but its faster then ruby sass and doesn't seg fault nearly as much so I
    feel comfortable converting.

--- server.js
+++ server.js
@@ -5,7 +5,9 @@ var config = require('./webpack.config');
 new WebpackDevServer(webpack(config), {
   publicPath: config.output.publicPath,
   hot: true,
-  historyApiFallback: true
+  historyApiFallback: true,
+  // noInfo: true,
+  color: true
 }).listen(3000, 'localhost', function (err, result) {
   if (err) {
     console.log(err);
