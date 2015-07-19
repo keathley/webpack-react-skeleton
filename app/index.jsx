14dd2f9351ce2e9d02f72226f61a2570d60fa5f8
Chris Keathley <c@keathley.io>
2015-07-18 22:37:59 -0400

    WIP

--- app/index.jsx
+++ app/index.jsx
@@ -1,17 +1,19 @@
+'use strict'
+
 var React = require('react')
-  , Router = require('react-router');
+  , Router = require('react-router')
 
 var App = require('./components/App')
   , Hello = require('./components/Hello')
 
-require('./styles/index.css');
+require('./styles/index.css')
 
 var routes = (
   <Router.Route name="app" path="/" handler={App}>
     <Router.DefaultRoute handler={Hello} />
   </Router.Route>
-);
+)
 
-Router.run(routes, Router.HistoryLocation, (Handler, state) => {
-  React.render(<Handler/>, document.body);
-});
+Router.run(routes, Router.HistoryLocation, (Handler) => {
+  React.render(<Handler />, document.body)
+})
